5df2e85cbe6c7f45d28d5492e86413b15e865f94
theteamisbest <abhithory@gmail.com>
2023-02-26 01:24:08 +0530

    ui functions added

--- extensions/your-rewards-post-purchase/src/index.jsx
+++ extensions/your-rewards-post-purchase/src/index.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import {
   extend,
   render,
@@ -16,14 +16,18 @@ import {
   Layout,
 } from '@shopify/post-purchase-ui-extensions-react';
 
-extend('Checkout::PostPurchase::ShouldRender', async ({storage}) => {
-  const postPurchaseOffer = {
-    "data":"this is datapostPurchaseOffer"
-  }
+extend('Checkout::PostPurchase::ShouldRender', async ({ storage }) => {
+  // const postPurchaseOffer = {
+  //   "data": "this is datapostPurchaseOffer"
+  // }
+  // await storage.update(postPurchaseOffer);
 
-  await storage.update(postPurchaseOffer);
+  const render = true;
+  console.log('====================================');
+  console.log("Here we can set render true false");
+  console.log('====================================');
 
-  return {render: true};
+  return { render };
 });
 
 render('Checkout::PostPurchase::Render', () => <App />);
@@ -36,35 +40,115 @@ export function App() {
     applyChangeset,
     done,
   } = useExtensionInput();
-  const [calculatedPurchase, setCalculatedPurchase] = useState();
+  const [pointsGenerated, setPointsGenerated] = useState(null);
+  const [isUserLogedin, setIsUserLogedin] = useState(null);
+
+  async function checkIsUserLogined() {
+    // featch api for checking user is logined or not
+
+
+    const reponse = {
+      isUserLogin: true,
+      otheruserDetails: {}
+    }
+    setTimeout(() => {
+      setIsUserLogedin(reponse.isUserLogin);
+      if (reponse.isUserLogin) giveRewardToUser();
+    }, 5000);
+
+  }
+
+  async function giveRewardToUser() {
+
+    const body = {
+      orderReferanceId: inputData.initialPurchase.referenceId,
+      shopDomain: inputData.shop.domain,
+      orderPriceData: inputData.initialPurchase.totalPriceSet.shopMoney,  // {amount,currencyCode}
+      shopifyToken: inputData.token
+    }
+    // console.log(body);
+
+
+    // callApiForGeneratingTokens
+    const reponse = {
+      pointsGenerated: body.orderPriceData.amount / 10,
+      userTotalPoints: body.orderPriceData.amount / 10 + 1201
+    }
+
+    console.log('====================================');
+    console.log("generating points");
+    console.log('====================================');
+
+    setTimeout(() => {
+      setPointsGenerated(reponse);
+      // done();
+    }, 5000);
+  }
 
   useEffect(() => {
+    setIsUserLogedin(null)
+    setPointsGenerated(null)
     console.log("Use effect");
-    console.log(inputData);
-    console.log(shop);
-  }, []);
+    checkIsUserLogined();
+  }, [inputData]);
 
   return (
     <BlockStack spacing="loose">
+
+
+      <Text size="medium" emphasized>
+        {isUserLogedin === null ? "We are checking user details" : (isUserLogedin === false ? "You are not logedin in our loyalty program." : "Thanks for using our loyalty program")}
+      </Text>
+
+      {pointsGenerated ?
+        <>
+          <Text size="medium" emphasized>
+            You earned {pointsGenerated?.pointsGenerated} YT token From this order;
+          </Text>
+          <Text size="medium" emphasized>
+            Now you total Points {pointsGenerated?.userTotalPoints} YT 😈
+          </Text>
+          <Button onPress={() => {
+            done();
+          }}>
+            Go to Order Status Page
+          </Button>
+          <Text size="small" emphasized>
+            You will automatically redirected to Next page after 5 secounds
+          </Text>
+        </>
+        :
+        (isUserLogedin === null || isUserLogedin === true ?
+          <>
             <Text size="medium" emphasized>
-              It&#39;s not too late to add this to your order
+              Loading...
             </Text>
+            <Text size="medium" emphasized>
+              Wait one secound Please. we are generating loyalty points for you.
+            </Text>
+          </>
+          :
+          (isUserLogedin === false &&
+            <>
 
-            <Button onPress={() => {
-              console.log("done");
-              done();
-            }}>
-              Done Done
-            </Button>
+              <Text size="medium" emphasized>
+                So, you can't get 255 loyalty points for this purchase.
+              </Text>
+              <Text size="medium" emphasized>
+                Login Nexttime for getting Loyalty Points
+              </Text>
+              <Button onPress={() => {
+            done();
+          }}>
+            Go to Order Status Page
+          </Button>
+            </>
+          )
+        )
+      }
 
-            <Button onPress={() => {
-              console.log("Get data");
-              console.log(inputData);
-              console.log(shop);
-            }}>
-              get Data
-            </Button>
-       
-    </BlockStack>
+
+
+    </BlockStack >
   );
 }
---
+++ receivedData/inputData.postpurchase.js
@@ -0,0 +1,76 @@
+const data = {
+    "token": "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzaG9waWZ5Iiwic3ViIjoiMjhjNmNkNjlhMDUwOGU3ZGFmMGM5ZDk3NDAxNThmYjMiLCJpbnB1dF9kYXRhIjp7InRva2VuIjpudWxsLCJzaG9wIjp7ImlkIjo3MjIyMzAzMTU4NywiZG9tYWluIjoidGVzdGluZy1jaGVja291dC11aTIubXlzaG9waWZ5LmNvbSIsIm1ldGFmaWVsZHMiOltdfSwiaW5pdGlhbFB1cmNoYXNlIjp7InJlZmVyZW5jZUlkIjoiMjhjNmNkNjlhMDUwOGU3ZGFmMGM5ZDk3NDAxNThmYjMiLCJjdXN0b21lcklkIjo2Nzg4MzY4ODkyMTk1LCJkZXN0aW5hdGlvbkNvdW50cnlDb2RlIjoiSU4iLCJ0b3RhbFByaWNlU2V0Ijp7InNob3BNb25leSI6eyJhbW91bnQiOiIzODk5Ljc4IiwiY3VycmVuY3lDb2RlIjoiSU5SIn0sInByZXNlbnRtZW50TW9uZXkiOnsiYW1vdW50IjoiMzg5OS43OCIsImN1cnJlbmN5Q29kZSI6IklOUiJ9fSwibGluZUl0ZW1zIjpbeyJ0b3RhbFByaWNlU2V0Ijp7InNob3BNb25leSI6eyJhbW91bnQiOiI3MjkuOTUiLCJjdXJyZW5jeUNvZGUiOiJJTlIifSwicHJlc2VudG1lbnRNb25leSI6eyJhbW91bnQiOiI3MjkuOTUiLCJjdXJyZW5jeUNvZGUiOiJJTlIifX0sInF1YW50aXR5IjoxLCJwcm9kdWN0Ijp7ImlkIjo4MTI2NDQwOTk3MTU1LCJ0aXRsZSI6IlRoZSBNdWx0aS1sb2NhdGlvbiBTbm93Ym9hcmQiLCJ2YXJpYW50Ijp7ImlkIjo0NDI4MTA2ODQ4Njk0NywidGl0bGUiOiJEZWZhdWx0IFRpdGxlIiwibWV0YWZpZWxkcyI6W119LCJtZXRhZmllbGRzIjpbXX0sInNlbGxpbmdQbGFuSWQiOm51bGx9LHsidG90YWxQcmljZVNldCI6eyJzaG9wTW9uZXkiOnsiYW1vdW50IjoiMjYyOS45NSIsImN1cnJlbmN5Q29kZSI6IklOUiJ9LCJwcmVzZW50bWVudE1vbmV5Ijp7ImFtb3VudCI6IjI2MjkuOTUiLCJjdXJyZW5jeUNvZGUiOiJJTlIifX0sInF1YW50aXR5IjoxLCJwcm9kdWN0Ijp7ImlkIjo4MTI2NDQxMDYyNjkxLCJ0aXRsZSI6IlRoZSAzcCBGdWxmaWxsZWQgU25vd2JvYXJkIiwidmFyaWFudCI6eyJpZCI6NDQyODEwNjg1NTI0ODMsInRpdGxlIjoiRGVmYXVsdCBUaXRsZSIsIm1ldGFmaWVsZHMiOltdfSwibWV0YWZpZWxkcyI6W119LCJzZWxsaW5nUGxhbklkIjpudWxsfV19LCJsb2NhbGUiOiJlbi1VUyIsImV4dGVuc2lvblBvaW50IjoiQ2hlY2tvdXQ6OlBvc3RQdXJjaGFzZTo6UmVuZGVyIiwidmVyc2lvbiI6InVuc3RhYmxlIn0sImlhdCI6MTY3NzM0NzA0M30.Q2gU7VnNfy6FpMXDjjIayaBjqcuPup7hLoaUk5T7ocU",
+    "shop": {
+        "id": 72223031587,
+        "domain": "testing-checkout-ui2.myshopify.com",
+        "metafields": []
+    },
+    "initialPurchase": {
+        "referenceId": "28c6cd69a0508e7daf0c9d9740158fb3",
+        "customerId": 6788368892195,
+        "destinationCountryCode": "IN",
+        "totalPriceSet": {
+            "shopMoney": {
+                "amount": "3899.78",
+                "currencyCode": "INR"
+            },
+            "presentmentMoney": {
+                "amount": "3899.78",
+                "currencyCode": "INR"
+            }
+        },
+        "lineItems": [
+            {
+                "totalPriceSet": {
+                    "shopMoney": {
+                        "amount": "729.95",
+                        "currencyCode": "INR"
+                    },
+                    "presentmentMoney": {
+                        "amount": "729.95",
+                        "currencyCode": "INR"
+                    }
+                },
+                "quantity": 1,
+                "product": {
+                    "id": 8126440997155,
+                    "title": "The Multi-location Snowboard",
+                    "variant": {
+                        "id": 44281068486947,
+                        "title": "Default Title",
+                        "metafields": []
+                    },
+                    "metafields": []
+                },
+                "sellingPlanId": null
+            },
+            {
+                "totalPriceSet": {
+                    "shopMoney": {
+                        "amount": "2629.95",
+                        "currencyCode": "INR"
+                    },
+                    "presentmentMoney": {
+                        "amount": "2629.95",
+                        "currencyCode": "INR"
+                    }
+                },
+                "quantity": 1,
+                "product": {
+                    "id": 8126441062691,
+                    "title": "The 3p Fulfilled Snowboard",
+                    "variant": {
+                        "id": 44281068552483,
+                        "title": "Default Title",
+                        "metafields": []
+                    },
+                    "metafields": []
+                },
+                "sellingPlanId": null
+            }
+        ]
+    },
+    "locale": "en-US",
+    "extensionPoint": "Checkout::PostPurchase::Render",
+    "version": "unstable"
+}
